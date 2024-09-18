89afeb5d1a6654053e9564dca7673c8941bd138f
Nguyễn Thành Nghĩa <amasimak98@gmail.com>
2024-09-18 18:31:11 +0700

    update new  web

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "DefaultCompany-Elevator-0.1.0";
-const contentToCache = [
-    "Build/Elevator_web.loader.js",
-    "Build/Elevator_web.framework.js.unityweb",
-    "Build/Elevator_web.data.unityweb",
-    "Build/Elevator_web.wasm.unityweb",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
